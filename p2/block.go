e2d314d9ac5b4747385341f728a6be5b9d2393bd
Alex Thompson <awt1088@gmail.com>
2019-02-22 01:17:50 -0800

    added func to hash, some of initial, and updated method signatures for encodeToJson and decodeFromJson

--- p2/block.go
+++ p2/block.go
@@ -1,6 +1,11 @@
 package p2
 
-import "github.com/palex88/Merkle-Patricia-Trie/p1"
+import (
+	"encoding/hex"
+	"github.com/palex88/Merkle-Patricia-Trie/p1"
+	"golang.org/x/crypto/sha3"
+	"time"
+)
 
 type Header struct {
 	Height     int32
@@ -12,19 +17,38 @@ type Header struct {
 
 type Block struct {
 	Header Header
-	Mpt p1.MerklePatriciaTrie
+	Value p1.MerklePatriciaTrie
 }
 
-func (block *Block) Initial()  {
+func (block *Block) HashBlock() string {
 
+	// TODO: Update mpt to have capitalized parameters
+	hashStr := string(block.Header.Height) + string(block.Header.Time) + block.Header.ParentHash + block.Value.Root + string(block.Header.Size)
+	bytes := sha3.Sum256([]byte(hashStr))
+	hash := hex.EncodeToString(bytes[:])
+
+	return hash
 }
 
-func (block *Block) DecodeFromJson()  {
-	
+func (block *Block) Initial(height int32, parentHash string, value p1.MerklePatriciaTrie)  {
+
+	block.Header.Height = height
+	block.Header.ParentHash = parentHash
+	block.Value = value
+	block.Header.Time = time.Now().Unix()
+	block.Header.Size = 0 // TODO: Needs to be updated to the proper value. Maybe check chain for last block height
+	block.Header.Hash = block.HashBlock()
 }
 
-func (block *Block) EncodeToJson() {
+func DecodeFromJson(jsonString string) Block {
 
+
+	return Block{}
+}
+
+func (block *Block) EncodeToJson() string {
+
+	return ""
 }
 
 
